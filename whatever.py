3e035b87c5a3cd37228b775b9c4f46d26ccf3c10
Lasse Keetz <l.t.keetz@geo.uio.no>
2022-01-14 21:50:11 +0100

    Added a useless function.

---
+++ whatever.py
@@ -0,0 +1,11 @@
+"""This script doesn't do anything useful"""
+
+def useless_function(some_string):
+    """This function is so useless, it doesn't even have documentation"""
+
+    print(some_string)
+    
+    return True
+
+if __name__ == "__main__":
+    useless_function("Hello world!")
